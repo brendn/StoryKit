a0dec0523a4de819842a8f09182ca57090abe8d4
brendn <brendn@users.noreply.github.com>
2022-08-25 19:29:20 -0400

    initial

---
+++ src/main/java/co/grandcircus/adventure/AdventureApplication.java
@@ -0,0 +1,13 @@
+package co.grandcircus.adventure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdventureApplication.class, args);
+	}
+
+}
---
+++ src/main/java/co/grandcircus/adventure/ServletInitializer.java
@@ -0,0 +1,13 @@
+package co.grandcircus.adventure;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AdventureApplication.class);
+	}
+
+}
